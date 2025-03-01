58c91b6bf8e4967906ea4dd09ced9a4b68b0895a
tsmialek <tsmialek.1@gmail.com>
2025-03-01 11:20:51 +0100

    feat: add, configure and test shadcn/ui library.

--- scheduler-frontend/vite.config.ts
+++ scheduler-frontend/vite.config.ts
@@ -1,15 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
